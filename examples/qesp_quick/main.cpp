c7ce60e00ea7c7a3c01b24772f72dc7c12b9e82b
dbzhang800 <dbzhang800@gmail.com>
2012-04-07 01:03:12 -0700

    Add quick example

---
+++ examples/qesp_quick/main.cpp
@@ -0,0 +1,45 @@
+/****************************************************************************
+** Copyright (c) 2012 Debao Zhang
+** All right reserved.
+** Web: http://code.google.com/p/qextserialport/
+**
+** Permission is hereby granted, free of charge, to any person obtaining
+** a copy of this software and associated documentation files (the
+** "Software"), to deal in the Software without restriction, including
+** without limitation the rights to use, copy, modify, merge, publish,
+** distribute, sublicense, and/or sell copies of the Software, and to
+** permit persons to whom the Software is furnished to do so, subject to
+** the following conditions:
+**
+** The above copyright notice and this permission notice shall be
+** included in all copies or substantial portions of the Software.
+**
+** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+** EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+** MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+** NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+** LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+** OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+** WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+**
+****************************************************************************/
+
+#include <QtGui/QGuiApplication>
+#include <QtQuick/QQuickView>
+#include <QtQml/QQmlEngine>
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+    const QString mainQmlApp = QLatin1String("qrc:/qesp_quick.qml");
+    QQuickView view;
+    view.setSource(QUrl(mainQmlApp));
+    view.setResizeMode(QQuickView::SizeRootObjectToView);
+
+    QObject::connect(view.engine(), SIGNAL(quit()), qApp, SLOT(quit()));
+
+    view.setGeometry(QRect(100, 100, 400, 600));
+    view.show();
+
+    return app.exec();
+}
